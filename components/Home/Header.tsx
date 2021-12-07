958a1c6d8b362032ad2099a2b37746ae0a69d472
isaacbatst <isaacbatst@gmail.com>
2021-12-06 23:40:43 -0300

    header

---
+++ components/Home/Header.tsx
@@ -0,0 +1,11 @@
+import styles from '../../styles/pages/home.module.scss'
+
+const Header: React.FC = () => {
+  return (
+    <header className={styles.header}>
+      <h1>Turma Fit</h1>
+    </header>
+  )
+}
+
+export default Header;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,31 +1,20 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
-import { useState } from 'react'
-import styles from '../styles/Home.module.css'
-import Image from 'next/image'
+import styles from '../styles/pages/home.module.scss'
 import Layout from '../components/Layout'
+import Header from '../components/Home/Header'
 
-async function fetchApi(endpoint: string) {
-  const response = await fetch(`/api/${endpoint}`)
-  if (!response.ok) {
-    throw new Error('Network response was not ok')
-  }
-
-  return response.json()
-}
 
 const Home: NextPage = () => {
   return (
     <Layout>
       <div className={styles.container}>
         <Head>
-          <title>Home</title>
+          <title>Turma Fit</title>
           <link rel="icon" href="/favicon.png" />
         </Head>
 
-        <section className={styles.main}>
-          Oi
-        </section>
+        <Header />
       </div>
     </Layout>
   )
