9fda369094602daaa58e4d3e7ea7cc0f24f248f9
isaacbatst <isaacbatst@gmail.com>
2021-12-07 22:13:34 -0300

    fix indentation

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -2,20 +2,20 @@ import Head from "next/head"
 
 const Layout: React.FC = ({ children }) => {
   return (
-      <>
-        <Head>
-          <link rel="icon" href="/favicon.ico" />
-          <meta
-            name="description"
-            content="Learn how to build a personal website using Next.js"
-          />
-          <meta name="og:title" content='TurmaFit' />
-          <meta name="twitter:card" content="summary_large_image" />
-        </Head>
-        <main>
-          { children }
-        </main>
-      </>
+    <>
+      <Head>
+        <link rel="icon" href="/favicon.ico" />
+        <meta
+          name="description"
+          content="Learn how to build a personal website using Next.js"
+        />
+        <meta name="og:title" content='TurmaFit' />
+        <meta name="twitter:card" content="summary_large_image" />
+      </Head>
+      <main>
+        { children }
+      </main>
+    </>
   )
 }
 
