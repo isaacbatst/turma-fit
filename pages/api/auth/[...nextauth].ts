9bd55b8ec2f2972ac0e0afcd771fbd02241f94cf
isaacbatst <isaacbatst@gmail.com>
2021-12-24 18:24:59 -0300

    create student on sign in

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -1,18 +1,41 @@
 import NextAuth from "next-auth";
-import Auth0Provider from 'next-auth/providers/auth0';
 import GoogleProvider from 'next-auth/providers/google';
+import { PrismaClient } from '@prisma/client'
 
 export default NextAuth({
   providers: [
-    Auth0Provider({
-      clientId: process.env.AUTH0_CLIENT_ID || '',
-      clientSecret: process.env.AUTH0_CLIENT_SECRET || '',
-      issuer: process.env.AUTH0_ISSUER 
-    }),
+    // Auth0Provider({
+    //   clientId: process.env.AUTH0_CLIENT_ID || '',
+    //   clientSecret: process.env.AUTH0_CLIENT_SECRET || '',
+    //   issuer: process.env.AUTH0_ISSUER 
+    // }),
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID || '',
       clientSecret: process.env.GOOGLE_CLIENT_SECRET || ''
     })
   ],
-  secret: process.env.JWT_SECRET
+  secret: process.env.JWT_SECRET,
+  callbacks: {
+    async signIn ({ user }) {
+      const prisma = new PrismaClient();
+
+      const student = await prisma.student.findFirst({
+        where: {
+          email: user.email || ''
+        }
+      })
+
+      if(!student) {
+        await prisma.student.create({
+          data: {
+            email: user.email || '',
+            name: user.name || '',
+            profile: user.image || '',
+          }
+        })
+      }
+
+      return true;
+    }
+  }
 })
