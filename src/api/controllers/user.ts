3fec0bddcdb97f5910387b8b7548b0b70d2a8a5b
isaacbatst <isaacbatst@DESKTOP-EFCKRRC.localdomain>
2022-03-10 00:39:24 -0300

    refactor: move try/catch to controller

--- src/api/controllers/user.ts
+++ src/api/controllers/user.ts
@@ -3,29 +3,34 @@ import { getToken, JWT } from "next-auth/jwt";
 import { patchUser } from '../services/user';
 
 export const patchUserHandler: NextApiHandler = async (req, res) => {
-  const { name } = req.body;
+  try {
+    const { name } = req.body;
 
-  const token = await getToken({ req }) as JWT;
+    const token = await getToken({ req }) as JWT;
 
-  if(!token.email){
-    return res.status(401).end();
-  }
+    if (!token.email) {
+      return res.status(401).end();
+    }
 
-  if (!name || name.trim().length === 0) {
-    return res.status(400).end();
-  }
+    if (!name || name.trim().length === 0) {
+      return res.status(400).end();
+    }
 
-  const { error, data } = await patchUser(token.email, { name });
+    const { error, data } = await patchUser(token.email, { name });
 
-  if(error) {
-    res.status(error.status).json({
-      message: error.message
-    })
-  }
+    if (error) {
+      res.status(error.status).json({
+        message: error.message
+      })
+    }
 
-  if(!data){
+    if (!data) {
+      res.status(500).end();
+    }
+
+    return res.status(200).json(data);
+  } catch (error) {
+    console.error(error)
     res.status(500).end();
   }
-
-  return res.status(200).json(data);
 }
--- src/pages/api/user/index.ts
+++ src/pages/api/user/index.ts
@@ -2,16 +2,11 @@ import { NextApiHandler } from 'next';
 import { patchUserHandler } from '../../../api/controllers/user';
 
 const handler: NextApiHandler = async (req, res) => {
-  try {
-    if (req.method === 'PATCH') {
-      return patchUserHandler(req, res);
-    }
-  
-    return res.status(405).end();
-  } catch (error) {
-    console.error(error);
-    return res.status(500).end(); 
+  if (req.method === 'PATCH') {
+    return patchUserHandler(req, res);
   }
+
+  return res.status(405).end();
 }
 
 
