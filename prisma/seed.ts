d732d93d196d2ea8609aa8992458018afa197108
isaacbatst <isaacbatst@DESKTOP-EFCKRRC.localdomain>
2022-03-14 21:59:27 -0300

    refactor: extract seedAll

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -9,40 +9,44 @@ async function main(){
   const prisma = new PrismaClient();
 
   try {
-    console.log('Start seeding...')
-
-    console.log('Seeding muscle groups')
-    await prisma.muscleGroup.createMany({
-      data: muscleGroups
-    })
-
-    console.log('Seeding equipments')
-    await prisma.equipment.createMany({
-      data: equipments
-    })
-
-    console.log('Seeding movements')
-    for(const movement of movements){
-      await prisma.movement.create({
-        data: movement
-      })
-    }
-
-    console.log('Seeding exercise techniques')
-    await prisma.exerciseTechnique.createMany({
-      data: exerciseTechniques
-    })
-
-    console.log('Seeding training planning types')
-    await prisma.trainingPlanningType.createMany({
-      data: trainingPlanningTypes
-    })
+    await seedAll(prisma);
 
   } catch (err) {
     console.error(err)
   }
 
   await prisma.$disconnect();
+}
+
+async function seedAll(prisma: PrismaClient){
+  console.log('Start seeding...')
+
+  console.log('Seeding muscle groups')
+  await prisma.muscleGroup.createMany({
+    data: muscleGroups
+  })
+
+  console.log('Seeding equipments')
+  await prisma.equipment.createMany({
+    data: equipments
+  })
+
+  console.log('Seeding movements')
+  for(const movement of movements){
+    await prisma.movement.create({
+      data: movement
+    })
+  }
+
+  console.log('Seeding exercise techniques')
+  await prisma.exerciseTechnique.createMany({
+    data: exerciseTechniques
+  })
+
+  console.log('Seeding training planning types')
+  await prisma.trainingPlanningType.createMany({
+    data: trainingPlanningTypes
+  })
 }
 
 main();
