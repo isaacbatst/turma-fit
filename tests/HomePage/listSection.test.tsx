1de0997eb073ea62a3ea39fe0c1719dc1a40b4c0
isaacbatst <isaacbatst@DESKTOP-EFCKRRC.localdomain>
2022-03-16 12:39:09 -0300

    style: remove empty lines

--- tests/HomePage/listSection.test.tsx
+++ tests/HomePage/listSection.test.tsx
@@ -4,7 +4,6 @@ import { setupServer } from 'msw/node';
 import { getPage } from 'next-page-tester';
 import * as Mocks from './listSection.mocks';
 
-
 const server = setupServer(
   // server.use should set custom responses for specific tests
   rest.get('/api/personal/advices', (req, res, ctx) => {
@@ -15,13 +14,9 @@ const server = setupServer(
   })
 )
 
-
-
 beforeAll(() => server.listen())
 afterEach(() => server.resetHandlers())
 afterAll(() => server.close())
-
-
 
 describe('Home Page - List Advices Section', () => {
 
