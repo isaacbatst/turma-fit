f0198a3822cace450aec7daf61d19d083d240a6c
isaacbatst <isaacbatst@DESKTOP-EFCKRRC.localdomain>
2022-03-16 15:21:46 -0300

    test: fix tests with render page func

--- tests/HomePage/listSection.test.tsx
+++ tests/HomePage/listSection.test.tsx
@@ -1,63 +1,65 @@
 import { act, screen } from '@testing-library/react';
-import { rest } from 'msw';
-import { setupServer } from 'msw/node';
 import { getPage } from 'next-page-tester';
-import * as Mocks from './listSection.mocks';
+import * as Mocks from '../mocks/listSection';
 
-const server = setupServer(
-  // server.use should set custom responses for specific tests
-  rest.get('/api/personal/advices', (req, res, ctx) => {
-    return res(ctx.json(Mocks.existingAdvices))
-  }),
-  rest.get(`${process.env.NEXTAUTH_URL}api/auth/session`, (req, res, ctx) => {
-    return res(ctx.json(Mocks.loggedSession))
+const renderPage = async () => await act(async () => {
+  const { render } = await getPage({
+    route: '/',
+    wrappers: 'tests/wrappers.tsx'
   })
-)
-
-beforeAll(() => server.listen())
-afterEach(() => server.resetHandlers())
-afterAll(() => server.close())
+  render();
+});
 
 describe('Home Page - List Advices Section', () => {
-
-  beforeEach(async () => {
-    await act(async () => {
-      const { render } = await getPage({
-        route: '/',
-      })
-      render();
-    });
-  })
-
   it('renders list section', async () => {
+    document.cookie = 'auth=personal-with-advice';
+    await renderPage();
+
     const section = await screen.findByRole('region', { name: "Seção de Listagem" })
     expect(section).toBeInTheDocument();
   })
 
   describe('Given User is a Personal', () => {
     describe('Given Personal has Advices', () => {
+      beforeAll(async () => {
+        document.cookie = 'auth=personal-with-advice';
+      })        
+
       it('renders list element', async () => {
+        await renderPage();
+
         const list = await screen.findByRole('list', { name: 'Lista de Alunos' })
         expect(list).toBeInTheDocument();
       })
       
       it('renders adviced student name', async () => {
-        const [ { student } ] = Mocks.existingAdvices;
-  
+        await renderPage();
+
         const [first] = await screen.findAllByRole('listitem')
-        
+
+        const [ { student } ] = Mocks.existingAdvices;
         expect(first).toHaveAccessibleName(student.user.name as string);
       })    
   
       it('renders adviced student without name', async () => {
+        await renderPage();
+
         const [_, second] = await screen.findAllByRole('listitem')
-        
         expect(second).toHaveAccessibleName('Nome não cadastrado');
       })  
     })
 
     describe('Given Personal has no Advices', () => {
+      beforeAll(async () => {
+        document.cookie = 'auth=personal-without-advice';
+      })   
+      
+      it('renders no advice', async () => {  
+        await renderPage()
 
+        const el = await screen.findByText('Nenhum aluno encontrado')
+        expect(el).toBeInTheDocument();
+      })
     })
   })
 })
