709d7cc7abf09c93427372b244ea98cc6a61bfd1
isaacbatst <isaacbatst@DESKTOP-EFCKRRC.localdomain>
2022-03-16 15:19:57 -0300

    feat: remove / redirect

--- next.config.js
+++ next.config.js
@@ -4,13 +4,4 @@ module.exports = {
   images: {
     domains: ['lh3.googleusercontent.com']
   },
-  async redirects(){
-    return [
-      {
-        source: '/',
-        destination: '/personal/advices',
-        permanent: false
-      }
-    ]
-  }
 }
