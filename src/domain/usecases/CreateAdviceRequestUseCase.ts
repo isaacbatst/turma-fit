bc8e22d33f23c9c7d475b3cf953aecf4142ac8e0
isaacbatst <isaacbatst@DESKTOP-EFCKRRC.localdomain>
2022-04-09 11:49:54 -0300

    refactor: remove input validation from usecase

--- src/domain/usecases/CreateAdviceRequestUseCase.ts
+++ src/domain/usecases/CreateAdviceRequestUseCase.ts
@@ -12,19 +12,6 @@ export interface CreateAdviceRequestDTO {
   createdAt: Date,
   status: AdviceRequestStatusType,
 }
-
-class CreateAdviceRequestPortValidator {
-  public static validate(port: CreateAdviceRequestUseCasePort){
-    if(!port.fromId || typeof port.fromId !== 'string'){
-      throw new Error('INVALID_FROM_ID');
-    }
-
-    if(!port.toId || typeof port.toId !== 'string'){
-      throw new Error('INVALID_TO_ID');
-    }  
-  }
-}
-
 export class CreateAdviceRequestUseCase {
   constructor(
     private adviceServiceRepository: AdviceRequestRepository,
@@ -32,8 +19,6 @@ export class CreateAdviceRequestUseCase {
   ) {}
   
   async execute(port: CreateAdviceRequestUseCasePort): Promise<CreateAdviceRequestDTO> {
-    CreateAdviceRequestPortValidator.validate(port);   
-    
     const adviceRequest = new AdviceRequest({})
 
     const fromProfile = await this.profileRepository.get(port.fromId);
