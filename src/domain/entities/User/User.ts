eb7d7fbf40d8ca41d049ccfcca43aa9781140abf
isaacbatst <isaacbatst@DESKTOP-EFCKRRC.localdomain>
2022-04-08 22:17:16 -0300

    feat: user entity

--- src/domain/entities/User/User.ts
+++ src/domain/entities/User/User.ts
@@ -1,23 +1,43 @@
+import { v4 } from "uuid";
+import WorkoutPlan from "../WorkoutPlan/WorkoutPlan";
+
+interface CreateUserEntityParams {
+  id?: string;
+  emailVerifiedAt?: Date;
+  name: string;
+  email: string;
+  image: string;
+  age: number
+}
 export class User {
   private id: string;
   private name: string;
   private email: string;
-  private emailVerifiedAt: Date;
+  private emailVerifiedAt: Date | null;
   private image: string;
   private age: number
-  private trainings: Training[]
-  private sentAdviceRequests: AdviceRequest[]        
-  private receivedAdviceRequests: AdviceRequest[]     
-  private userTrainingPlannings: UserTrainingPlanning[]
-}
+  // private workoutPlans: WorkoutPlan[]
+  // private sentAdviceRequests: AdviceRequest[]        
+  // private receivedAdviceRequests: AdviceRequest[]     
+  // private userTrainingPlannings: UserTrainingPlanning[]
 
-export class Personal extends User {
-  private advices: Advice[]
-}
+  constructor(params: CreateUserEntityParams) {
+    this.id = params.id || v4();
+    this.emailVerifiedAt = params.emailVerifiedAt || null;
+    this.name = params.name;
+    this.email = params.email;
+    this.image = params.image;
+    this.age = params.age;
+  }
+} 
 
-export class Student extends User {
-  private measures: Measurement[]
-  private pictures: Picture[]
-  private advices : Advice[]
-}
+// export class Personal extends User {
+//   private advices: Advice[]
+// }
+
+// export class Student extends User {
+//   private measures: Measurement[]
+//   private pictures: Picture[]
+//   private advices : Advice[]
+// }
 
