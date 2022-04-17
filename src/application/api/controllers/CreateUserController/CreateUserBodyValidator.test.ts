fa22002e5fc1d92aac1f747deacb8568105022ec
isaacbatst <isaacbatst@DESKTOP-EFCKRRC.localdomain>
2022-04-16 22:46:56 -0300

    test: CreateUserBodyValidator invalid password

--- src/application/api/controllers/CreateUserController/CreateUserBodyValidator.test.ts
+++ src/application/api/controllers/CreateUserController/CreateUserBodyValidator.test.ts
@@ -1,3 +1,6 @@
+import { BodyValidator } from "@application/api/interfaces";
+import { CreateUserUseCasePort } from "@domain/usecases/CreateUserUseCase/CreateUserUseCase";
+
 export class ValidationError extends Error {
   constructor(
     public message: string
@@ -10,10 +13,52 @@ export class ValidationError extends Error {
   }
 }
 
-describe('CreateUserBodyValidator', () => {
-  describe('Given it receives an invalid password', () => {
-    it('should return INVALID_PASSWORD error', () => {
+class CreateUserBodyValidator implements BodyValidator<CreateUserUseCasePort> {
+  validate(body: Record<string, any>): CreateUserUseCasePort {
+    const { password } = body
 
+    if(!password || typeof password !== 'string' || password.length < 8) {
+      throw new ValidationError('INVALID_PASSWORD')
+    }
+
+    return {
+      password,
+    }
+  }
+}
+
+describe('CreateUserBodyValidator', () => {
+  describe('Given it receives no password', () => {
+    it('should throw INVALID_PASSWORD error', () => {
+      const createUserBodyValidator = new CreateUserBodyValidator();
+      
+      expect(() => {
+        createUserBodyValidator.validate({});
+      }).toThrowError('INVALID_PASSWORD')
+    })
+  })
+
+  describe('Given it receives a password that is not a string', () => {
+    it('should throw INVALID_PASSWORD error', () => {
+      const createUserBodyValidator = new CreateUserBodyValidator();
+      
+      expect(() => {
+        createUserBodyValidator.validate({
+          password: false
+        });
+      }).toThrowError('INVALID_PASSWORD')
+    })
+  })
+
+  describe('Given it receives a password with length < 8', () => {
+    it('should throw INVALID_PASSWORD error', () => {
+      const createUserBodyValidator = new CreateUserBodyValidator();
+      
+      expect(() => {
+        createUserBodyValidator.validate({
+          password: 'small',
+        });
+      }).toThrowError('INVALID_PASSWORD')
     })
   })
 })
