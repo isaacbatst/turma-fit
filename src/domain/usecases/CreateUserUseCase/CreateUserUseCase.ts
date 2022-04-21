e8db9bffbd3137cfd543f834af29103ab79c10df
isaacbatst <isaacbatst@DESKTOP-EFCKRRC.localdomain>
2022-04-20 23:38:20 -0300

    fix: email validation regex

--- src/domain/usecases/CreateUserUseCase/CreateUserUseCase.ts
+++ src/domain/usecases/CreateUserUseCase/CreateUserUseCase.ts
@@ -34,7 +34,7 @@ interface CreateUserUseCasePortValidated {
   password: string,
 }
 class CreateUserPortValidator {
-  static readonly EMAIL_REGEX = /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/gi
+  static readonly EMAIL_REGEX = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
 
   static validate(port: CreateUserUseCasePort): CreateUserUseCasePortValidated {
     if(port.profile !== PROFILE_TYPES.PERSONAL && port.profile !== PROFILE_TYPES.STUDENT) {
