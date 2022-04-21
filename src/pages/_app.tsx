60a1f827127c14d745befbbf9d3b5a15a8f2c8dd
isaacbatst <isaacbatst@DESKTOP-EFCKRRC.localdomain>
2022-04-21 18:24:16 -0300

    refactor: reset app.tsx

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,69 +1,14 @@
-import { NextComponentType, NextPageContext } from 'next';
-import { Session } from 'next-auth';
-import { getSession, SessionProvider } from 'next-auth/react';
-import type { AppContext, AppProps } from 'next/app';
-import App from 'next/app';
+import type { AppProps } from 'next/app';
 import { Provider } from 'react-redux';
-import LoadingPage from '../components/common/LoadingPage';
-import { checkShouldFillProfile } from '../lib/user';
 import { store } from '../store/index';
 import '../styles/common/global.scss';
 
-type MyAppProps = AppProps & {
-  pageProps: {
-    session: Session,
-    redirect?: boolean
-  }
-}
-function MyApp({ Component, pageProps: { session, redirect, ...pageProps } }: MyAppProps) {
+function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <SessionProvider session={session}>
-      <Provider store={store}>
-        <Component {...pageProps} />
-      </Provider>
-    </SessionProvider>
+    <Provider store={store}>
+      <Component {...pageProps} />
+    </Provider>
   )
 }
-MyApp.getInitialProps = async (appContext: AppContext) => {
-  const appProps = await App.getInitialProps(appContext);
-  const session = await getSession(appContext.ctx)
-  appProps.pageProps.session = session;
-  
-  const { ctx: { res }, router } = appContext;
-  
-  
-  const pathToRedirect = shouldRedirectTo(session, router.pathname);
-
-  if(pathToRedirect && res){
-
-    res.writeHead(307, { Location: pathToRedirect })
-    res.end()
-    return { ...appProps }
-  }
-
-  if(pathToRedirect) {
-    router.push(pathToRedirect)
-    return { ...appProps }
-  }
-
-  return { ...appProps }
-}
-
-function shouldRedirectTo(session: Session | null, pathname: string){
-  if(!session){
-    return '/'
-  }
-
-  const shouldFillProfile = checkShouldFillProfile(session.user)
-
-  if (shouldFillProfile&& pathname !== '/fill-profile' ) {
-    return '/fill-profile'
-  }
-
-  if (!shouldFillProfile && pathname === '/fill-profile') {
-    return '/'
-  }
-}
-
 
 export default MyApp;
