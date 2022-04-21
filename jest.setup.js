b066203c6b0803cf7b45cb3a05b81e5fa01e6eeb
isaacbatst <isaacbatst@DESKTOP-EFCKRRC.localdomain>
2022-04-21 17:36:41 -0300

    refactor: remove testes de api antigos

--- jest.setup.js
+++ jest.setup.js
@@ -1,9 +1,4 @@
 import fetch from 'cross-fetch';
-import server from './tests/server';
 import '@testing-library/jest-dom';
-
-beforeAll(() => server.listen())
-afterEach(() => server.resetHandlers())
-afterAll(() => server.close())
 
 global.fetch = fetch;
