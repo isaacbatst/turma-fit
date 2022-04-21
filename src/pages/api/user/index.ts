2cdfdc632e7f501138fee6569fe82d807581e31a
isaacbatst <isaacbatst@DESKTOP-EFCKRRC.localdomain>
2022-04-21 18:23:46 -0300

    fix: remove old imports

--- src/pages/api/user/index.ts
+++ src/pages/api/user/index.ts
@@ -1,12 +1,7 @@
 import { makeCreateUserHandler } from '@application/api/factories/CreateUserHandler';
 import { NextApiHandler } from 'next';
-import { patchUserHandler } from '../../../api/controllers/user';
 
 const handler: NextApiHandler = async (req, res) => {
-  if (req.method === 'PATCH') {
-    return patchUserHandler(req, res);
-  }
-
   if(req.method === 'POST') {
     const createUserHandler = makeCreateUserHandler();
 
