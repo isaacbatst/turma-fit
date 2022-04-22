6b8ffd00ca2663ea9561b82205f9e1921e98d215
isaacbatst <isaacbatst@DESKTOP-EFCKRRC.localdomain>
2022-04-21 23:00:44 -0300

    test: create user with generated uuid

--- src/domain/usecases/CreateUserUseCase/CreateUserUseCase.test.ts
+++ src/domain/usecases/CreateUserUseCase/CreateUserUseCase.test.ts
@@ -4,13 +4,15 @@ import { EncrypterMock, ProfileRepositoryMock, UserRepositoryMock } from "../_mo
 import { CreateUserUseCasePort } from "./CreateUserPortValidator";
 import { CreateUserService } from "./CreateUserUseCase";
 
+const UUID_MOCK = 'any-uuid'
+
 jest.mock('uuid',  () => {
   const originalModule = jest.requireActual('uuid');
 
   return {
     __esModule: true,
     ...originalModule,
-    v4: jest.fn(() => 'any-uuid'),
+    v4: jest.fn(() => UUID_MOCK),
   };
 })
 
@@ -32,45 +34,51 @@ const STUDENT_USER_CREATE_DATA_MOCK: CreateUserUseCasePort = {
   password: 'any-password'
 }
 
-describe('CreateUserUseCase', () => {
-  it('should call userRepository with proper params', async() => {
-    const userRepository = new UserRepositoryMock();
-    const profileRepository = new ProfileRepositoryMock();
-    const encrypter = new EncrypterMock();
-    const createUserUseCase = new CreateUserService(userRepository, profileRepository, encrypter);
+const makeSut = () => {
+  const userRepository = new UserRepositoryMock();
+  const profileRepository = new ProfileRepositoryMock();
+  const encrypter = new EncrypterMock();
+  const createUserUseCase = new CreateUserService(userRepository, profileRepository, encrypter);
 
+  return {
+    userRepository, 
+    profileRepository,
+    encrypter,
+    createUserUseCase
+  }
+}
+
+describe('CreateUserUseCase', () => {
+  it('should call userRepository.create with proper params', async() => {
+    const { createUserUseCase, userRepository } = makeSut();
     await createUserUseCase.execute(PERSONAL_USER_CREATE_DATA_MOCK)
 
     const expectedUserParameter = new User({
-      age: PERSONAL_USER_CREATE_DATA_MOCK.age,
-      name: PERSONAL_USER_CREATE_DATA_MOCK.name,
-      email: PERSONAL_USER_CREATE_DATA_MOCK.email,
-      password: await encrypter.hash(PERSONAL_USER_CREATE_DATA_MOCK.password),
-      image: PERSONAL_USER_CREATE_DATA_MOCK.image,
+      ...PERSONAL_USER_CREATE_DATA_MOCK,
+      password: EncrypterMock.HASHED_VALUE,
     })
 
     expect(userRepository.create).toHaveBeenCalledWith(expectedUserParameter)
   })
 
   it('should create a user with personal profile', async () => {
-    const userRepository = new UserRepositoryMock();
-    const profileRepository = new ProfileRepositoryMock();
-    const encrypter = new EncrypterMock();
-    const createUserUseCase = new CreateUserService(userRepository, profileRepository, encrypter);
-
+    const { createUserUseCase } = makeSut();
     const { profile } = await createUserUseCase.execute(PERSONAL_USER_CREATE_DATA_MOCK);
     
     expect(profile.type).toBe(PROFILE_TYPES.PERSONAL)
   })
 
   it('should create a user with student profile', async () => {
-    const userRepository = new UserRepositoryMock();
-    const profileRepository = new ProfileRepositoryMock();
-    const encrypter = new EncrypterMock();
-    const createUserUseCase = new CreateUserService(userRepository, profileRepository, encrypter);
-
+    const { createUserUseCase } = makeSut();
     const { profile } = await createUserUseCase.execute(STUDENT_USER_CREATE_DATA_MOCK);
     
     expect(profile.type).toBe(PROFILE_TYPES.STUDENT)
+  })
+
+  it('should create a user with generated uuid', async () => {
+    const { createUserUseCase } = makeSut();
+    const { user } = await createUserUseCase.execute(PERSONAL_USER_CREATE_DATA_MOCK);
+
+    expect(user.id).toBe(UUID_MOCK)
   })
 })
