7040dc85e682123b092e7e0b3c6191e85ac2502d
isaacbatst <isaacbatst@DESKTOP-EFCKRRC.localdomain>
2022-04-21 23:42:06 -0300

    refactor: bcrypt to adapter

--- src/application/api/adapters/BcryptEncrypterAdapter.ts
+++ src/application/api/adapters/BcryptEncrypterAdapter.ts
@@ -2,7 +2,7 @@ import { Encrypter } from "@domain/common/Encrypter";
 import bcrypt from 'bcrypt';
 
 const SALT_ROUNDS = 10; 
-export class BcryptEncrypter implements Encrypter {
+export class BcryptEncrypterAdapter implements Encrypter {
   compare(value: string, hashedValue: string): Promise<boolean> {
     return bcrypt.compare(value, hashedValue)
   }
--- src/application/api/factories/CreateUserHandler.ts
+++ src/application/api/factories/CreateUserHandler.ts
@@ -2,20 +2,23 @@ import { CreateUserService } from "@domain/usecases/CreateUserUseCase/CreateUser
 import { PrismaProfileRepository } from "@infra/persistence/prisma/adapters/PrismaProfileRepository";
 import { PrismaUserRepository } from "@infra/persistence/prisma/adapters/PrismaUserRepository";
 import { prisma } from "src/lib/prisma";
-import { BcryptEncrypter } from "src/utils/BcryptEncrypter";
+import { BcryptEncrypterAdapter } from "@application/api/adapters/BcryptEncrypterAdapter";
 import { NextApiHandlerAdapter } from "../adapters/NextApiHandlerAdapter";
 import { CreateUserBodyValidator } from "../controllers/CreateUserController/CreateUserBodyValidator";
 import { CreateUserController } from "../controllers/CreateUserController/CreateUserController";
+import { JwtTokenGeneratorAdapter } from "../adapters/JwtTokenGeneratorAdapter";
 
 export function makeCreateUserHandler() {
   const userRepository = new PrismaUserRepository(prisma);
   const profileRepository = new PrismaProfileRepository(prisma);
-  const bcryptEncrypter = new BcryptEncrypter();
+  const bcryptEncrypter = new BcryptEncrypterAdapter();
+  const jwtGenerator = new JwtTokenGeneratorAdapter();
 
   const createUserService = new CreateUserService(
     userRepository,
     profileRepository,
-    bcryptEncrypter
+    bcryptEncrypter,
+    jwtGenerator
   );
   const createUserBodyValidator = new CreateUserBodyValidator();
 
