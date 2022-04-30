c0b0272261f4d7718d8369f84c01b7ab3b5d299e
isaacbatst <isaacbatst@DESKTOP-EFCKRRC.localdomain>
2022-04-30 16:46:41 -0300

    feat: measurement entity

---
+++ src/domain/entities/User/Measurement.ts
@@ -0,0 +1,17 @@
+export interface Measurement {
+  createdAt       :  Date       
+  id              :  string         
+  weight          :  number
+  shoulders       :  number
+  chest           :  number
+  waist           :  number
+  hips            :  number
+  arm_left        :  number
+  arm_right       :  number
+  forearm_left    :  number
+  forearm_right   :  number
+  thigh_left      :  number
+  thigh_right     :  number
+  calf_left       :  number
+  calf_right      :  number
+}
