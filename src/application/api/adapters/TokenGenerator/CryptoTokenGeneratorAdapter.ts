9d3c8874c50720a0acb8146669b1b40251482dc7
Isaac Batista <isaacbatst@gmail.com>
2022-05-10 12:18:34 -0300

    refactor: remove logs

--- src/application/api/adapters/TokenGenerator/CryptoTokenGeneratorAdapter.ts
+++ src/application/api/adapters/TokenGenerator/CryptoTokenGeneratorAdapter.ts
@@ -3,9 +3,6 @@ import crypto from 'node:crypto';
 
 export class CryptoTokenGeneratorAdapter implements TokenGenerator {
   generate(): string {
-    console.log(crypto.randomBytes(16).toString('hex'));
-    console.log(crypto.randomBytes(16).toString('base64'));
-
     return crypto.randomBytes(20).toString('hex');
   }
 }
