264a740c5dc42b654fdb317008542ce493f82dba
Isaac Batista <isaacbatst@gmail.com>
2022-05-15 10:34:01 -0300

    refactor: remove unused login page

--- src/pages/login.tsx
+++
@@ -1,6 +0,0 @@
-import LoginPage, { getLoginPageServerSideProps } from '@application/frontend/pages/LoginPage/LoginPage';
-import type { GetServerSideProps } from 'next';
-
-export const getServerSideProps: GetServerSideProps = getLoginPageServerSideProps
-
-export default LoginPage;
