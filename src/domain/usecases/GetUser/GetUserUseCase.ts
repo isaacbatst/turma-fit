172cc1b45f19b7f41260a28a47766d0a0d9cddfb
Isaac Batista <isaacbatst@gmail.com>
2022-05-14 23:08:56 -0300

    feat: get user use case

---
+++ src/domain/usecases/GetUser/GetUserUseCase.ts
@@ -0,0 +1,40 @@
+import { UseCase } from "@domain/common/UseCase";
+import { User } from "@domain/entities/User/User";
+
+interface GetUserUseCasePort {
+  token: string
+}
+
+interface GetUserUseCaseDTO {
+  user: {
+    id: string,
+    name: string,
+  }
+}
+
+export interface IGetUserUseCase extends UseCase<GetUserUseCasePort, GetUserUseCaseDTO> {}
+
+interface GetUserRepository {
+  getByToken(token: string): Promise<User | null>
+}
+
+export class GetUserUseCase implements IGetUserUseCase {
+  constructor(
+    private userRepository: GetUserRepository
+  ) {}
+
+  async execute(port: GetUserUseCasePort): Promise<GetUserUseCaseDTO> {
+    const user = await this.userRepository.getByToken(port.token)
+
+    if(!user) {
+      throw new Error('USER_NOT_FOUND')
+    }
+
+    return {
+      user: {
+        id: user.getId(),
+        name: user.getName(),
+      }
+    }
+  }
+}
