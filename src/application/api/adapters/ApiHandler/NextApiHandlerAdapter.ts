93efc755bb4fc74e26268622929693a445331567
Isaac Batista <isaacbatst@gmail.com>
2022-05-15 00:31:14 -0300

    refactor: use cookie classes

--- src/application/api/adapters/ApiHandler/NextApiHandlerAdapter.ts
+++ src/application/api/adapters/ApiHandler/NextApiHandlerAdapter.ts
@@ -1,5 +1,5 @@
+import { CookiesHandler } from "@application/api/common/CookiesHandler";
 import { NextApiRequest, NextApiResponse } from "next";
-import { serialize, CookieSerializeOptions } from 'cookie'
 import { Controller, HttpRequest } from "../../interfaces";
 
 export class NextApiHandlerAdapter {
@@ -11,7 +11,8 @@ export class NextApiHandlerAdapter {
     const httpRequest: HttpRequest = {
       body: req.body,
       query: req.query,
-      headers: req.headers
+      headers: req.headers,
+      cookies: req.cookies
     }
 
     const { statusCode, body, cookies } = await this.controller.handle(httpRequest);
@@ -20,26 +21,9 @@ export class NextApiHandlerAdapter {
     if(!cookies) return res.status(statusCode).json(body);
 
     Object.entries(cookies).forEach(([key, value]) => {
-      this.setCookie(res, key, value);
+      CookiesHandler.setCookie(res, key, value);
     })
 
     return res.status(statusCode).json(body);
-  }
-
-  private setCookie = (
-    res: NextApiResponse,
-    name: string,
-    value: unknown,
-    options: CookieSerializeOptions = {}
-  ) => {
-    const stringValue =
-      typeof value === 'object' ? 'j:' + JSON.stringify(value) : String(value)
-  
-    if (options.maxAge) {
-      options.expires = new Date(Date.now() + options.maxAge)
-      options.maxAge /= 1000
-    }
-  
-    res.setHeader('Set-Cookie', serialize(name, stringValue, options))
   }
 }
