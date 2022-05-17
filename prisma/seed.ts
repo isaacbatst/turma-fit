ab20c00ff14cf9345d8af75cf588b8ad760b16f1
Isaac Batista <isaacbatst@gmail.com>
2022-05-16 22:22:29 -0300

    refactor: remove old schema dumps and seeds

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,19 +1,11 @@
 import { PrismaClient } from "@prisma/client";
-import equipments from "./seedData/equipments";
-import exercises from "./seedData/exercises";
-import exerciseTechniques from "./seedData/exerciseTechniques";
-import movements from "./seedData/movements";
-import muscleGroups from "./seedData/muscleGroups";
-import trainingPlannings from "./seedData/trainingPlanning";
-import trainingPlanningTypes from "./seedData/trainingPlanningTypes";
-import trainings from "./seedData/trainings";
-import users from "./seedData/users";
 
 async function main(){
   const prisma = new PrismaClient();
 
   try {
-    await seedAll(prisma);
+    console.log('seeding is bypassed...');
+    // await seedAll(prisma);
 
   } catch (err) {
     console.error(err)
@@ -22,66 +14,8 @@ async function main(){
   await prisma.$disconnect();
 }
 
-async function seedAll(prisma: PrismaClient){
-  console.log('Start seeding...')
-
-  console.log('Seeding muscle groups')
-  await prisma.muscleGroup.createMany({
-    data: muscleGroups
-  })
-
-  console.log('Seeding equipments')
-  await prisma.equipment.createMany({
-    data: equipments
-  })
-
-  console.log('Seeding movements')
-  for(const movement of movements){
-    await prisma.movement.create({
-      data: movement
-    })
-  }
-
-  console.log('Seeding exercise techniques')
-  await prisma.exerciseTechnique.createMany({
-    data: exerciseTechniques
-  })
-
-  console.log('Seeding training planning types')
-  await prisma.trainingPlanningType.createMany({
-    data: trainingPlanningTypes
-  })
-
-  console.log('Seeding exercises')
-  for(const exercise of exercises){
-    await prisma.exercise.create({
-      data: exercise
-    })
-  }
-
-  console.log('Seeding trainings')
-  for(const training of trainings){
-    await prisma.training.create({
-      data: training
-    })
-  }
-
-
-  console.log('Seeding training plannings')
-  for(const trainingPlanning of trainingPlannings){
-    await prisma.trainingPlanning.create({
-      data: trainingPlanning
-    })
-  }
-
-  console.log('Seeding users')
-  for(const user of users){
-    await prisma.user.create({
-      data: user
-    })
-  }
-
+// async function seedAll(prisma: PrismaClient){
   
-}
+// }
 
 main();
