14266fbb4adac1738806429118a8df668281b86e
Isaac Batista <isaacbatst@gmail.com>
2022-06-12 11:20:25 -0300

    feat: remove exercise button

--- src/application/frontend/pages/HomePage/components/CreateWorkoutPlanForm/Workout/SetInputs/Exercises.tsx
+++ src/application/frontend/pages/HomePage/components/CreateWorkoutPlanForm/Workout/SetInputs/Exercises.tsx
@@ -1,5 +1,5 @@
 import { useAppDispatch, useAppSelector } from '@application/frontend/store/hooks';
-import { addExerciseAction, selectExercises } from '@application/frontend/store/slices/CreateWorkoutPlanForm';
+import { addExerciseAction, removeExerciseAction, selectExercises } from '@application/frontend/store/slices/CreateWorkoutPlanForm';
 import React from 'react';
 import EquipmentSelect from './EquipmentSelect';
 import GripRadio from './GripRadio';
@@ -19,11 +19,21 @@ const Exercises: React.FC<Props> = ({ setIndex, workoutIndex }) => {
       <h5>Exercícios</h5>
       <button type="button" onClick={() => dispatch(addExerciseAction({ setIndex, workoutIndex }))} >+ Exercício</button>
       {
-        exercises.map(exercise => (
+        exercises.map((exercise, exerciseIndex) => (
           <div key={exercise.id}>
             <MovementSelect />
             <EquipmentSelect />
             <GripRadio />
+            <button 
+              type="button" 
+              onClick={() => dispatch(removeExerciseAction({
+                exerciseIndex,
+                setIndex,
+                workoutIndex
+              }))}
+            >
+              Remover exercício
+            </button>
           </div>
         ))
       }
