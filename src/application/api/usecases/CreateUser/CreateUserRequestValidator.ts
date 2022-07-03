54f82e96c58477fcfcb439cd4b42cc0f34a67007
Isaac Batista <isaacbatst@gmail.com>
2022-07-03 10:33:48 -0300

    fix: replace age for birthdate on request validator

--- src/application/api/usecases/CreateUser/CreateUserRequestValidator.ts
+++ src/application/api/usecases/CreateUser/CreateUserRequestValidator.ts
@@ -6,28 +6,24 @@ export interface CreateUserValidRequest {
   name: string,
   email: string,
   image: string,
-  age: number,
+  birthdate: string,
   profile: string,
   password: string,
 }
 export class CreateUserRequestValidator implements RequestValidator<CreateUserValidRequest> {
   validate(request: Record<string, any>): CreateUserValidRequest {
-    const { password, age, email, image, name, profile } = request;
+    const { password, birthdate, email, image, name, profile } = request;
     
     if(!password || typeof password !== 'string') {
       throw new ValidationError(CreateUserRequestErrors.INVALID_PASSWORD)
     }
 
-    if(!age || typeof age !== 'number') {
-      throw new ValidationError(CreateUserRequestErrors.INVALID_AGE)
+    if(!birthdate || typeof birthdate !== 'string') {
+      throw new ValidationError(CreateUserRequestErrors.INVALID_BIRTHDATE)
     }
 
     if(!email || typeof email !== 'string') {
       throw new ValidationError(CreateUserRequestErrors.INVALID_EMAIL)
-    }
-
-    if(!image || typeof image !== 'string') {
-      throw new ValidationError(CreateUserRequestErrors.INVALID_IMAGE)
     }
 
     if(!name || typeof name !== 'string') {
@@ -40,7 +36,7 @@ export class CreateUserRequestValidator implements RequestValidator<CreateUserVa
 
     return {
       password,
-      age,
+      birthdate,
       email,
       image,
       name,
